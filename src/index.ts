762aa001a8ae17b9c88290ea3d02673e2a1728db
svashchenko8877 <vashchenkosergey96@icloud.com>
2024-11-24 12:20:53 -0800

    Infrastructure improvement

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,22 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 5432,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 app.get('/', async (req: Request, res: Response) => {
-    try {
-        const result = await pool.query('SELECT * FROM cars');
-        res.json(result.rows);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Error retrieving data from database');
-    }
+  try {
+    const result = await pool.query('SELECT * FROM cars');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error retrieving data from database');
+  }
 });
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
-
